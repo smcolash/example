eff8dd10fa328ce05fd620ccf8eec04899ce5db5
Scott McOlash <s.mcolash@ieee.org>
2020-12-02 02:07:00 +0000

    minor changes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main (int argc, char *argv[])
 {
     std::cout << "example" << std::endl;
     std::cout << "simple edit" << std::endl;
-    
+    std::cout << "another edit" << std::endl;
+
     return (0);
 }
